da2a052ffa59af2b289c88b43c764dbb1a0936f4
Bryan-ro <bryan.grocha@sptech.school>
2025-09-26 22:23:02 -0300

    src upload

---
+++ src/main/java/com/solarize/solarize_web_backend/SolarizeWebBackendApplication.java
@@ -0,0 +1,13 @@
+package com.solarize.solarize_web_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolarizeWebBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolarizeWebBackendApplication.class, args);
+	}
+
+}
