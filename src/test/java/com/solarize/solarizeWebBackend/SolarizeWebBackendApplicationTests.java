84e13aaaefba419dac938d3ff60c02fe897cd097
Bryan Rocha <160238490+BryanRO-SpTech@users.noreply.github.com>
2025-10-21 18:29:59 -0300

    Add test mail username property to test config
    Introduced @TestPropertySource to set 'spring.mail.username' for tests in SolarizeWebBackendApplicationTests. This helps ensure consistent test environment configuration.

--- src/test/java/com/solarize/solarizeWebBackend/SolarizeWebBackendApplicationTests.java
+++ src/test/java/com/solarize/solarizeWebBackend/SolarizeWebBackendApplicationTests.java
@@ -2,8 +2,12 @@ package com.solarize.solarizeWebBackend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+        "spring.mail.username=test@email.com"
+})
 class SolarizeWebBackendApplicationTests {
 
 	@Test
