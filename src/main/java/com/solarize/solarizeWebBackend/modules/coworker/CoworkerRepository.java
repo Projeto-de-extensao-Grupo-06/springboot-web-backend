c1faa2481046873285e10342ba6afa6296efbcb7
victorsantos41 <victor.santos047@sptech.school>
2025-11-08 18:34:39 -0300

    add: findByPhone method

--- src/main/java/com/solarize/solarizeWebBackend/modules/coworker/CoworkerRepository.java
+++ src/main/java/com/solarize/solarizeWebBackend/modules/coworker/CoworkerRepository.java
@@ -1,6 +1,7 @@
 package com.solarize.solarizeWebBackend.modules.coworker;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -11,6 +12,7 @@ import java.util.Optional;
 @Repository
 public interface CoworkerRepository extends JpaRepository<Coworker, Long> {
     Optional<Coworker> findByEmail(String email);
+    Optional<Coworker> findByPhone(String phone);
 
     @Query("SELECT c FROM Coworker c JOIN FETCH c.permission WHERE c.email = :email")
     Optional<Coworker> findByEmailWithPermissions(String email);
