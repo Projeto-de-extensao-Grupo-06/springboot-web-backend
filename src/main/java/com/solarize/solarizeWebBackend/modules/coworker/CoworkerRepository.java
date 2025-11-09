8e6c8bb76ae0261ac3ffdc08ac7832492de80ff1
victorsantos41 <victor.santos047@sptech.school>
2025-11-09 17:35:06 -0300

    add: Boolean methods for isActive validation

--- src/main/java/com/solarize/solarizeWebBackend/modules/coworker/CoworkerRepository.java
+++ src/main/java/com/solarize/solarizeWebBackend/modules/coworker/CoworkerRepository.java
@@ -1,18 +1,22 @@
 package com.solarize.solarizeWebBackend.modules.coworker;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface CoworkerRepository extends JpaRepository<Coworker, Long> {
-    Optional<Coworker> findByEmail(String email);
-    Optional<Coworker> findByPhone(String phone);
+    Optional<Coworker> findByEmailAndIsActiveTrue(String email);
+    Optional<Coworker> findByPhoneAndIsActiveTrue(String phone);
+
+    List<Coworker> findAllByisActiveTrue();
+    Optional<Coworker> findByIdAndIsActiveTrue(Long id);
+    boolean existsByEmailAndIsActiveTrue(String email);
+    boolean existsByPhoneAndIsActiveTrue(String Phone);
+    boolean existsByIsActive(Boolean isActive);
 
     @Query("SELECT c FROM Coworker c JOIN FETCH c.permission WHERE c.email = :email")
     Optional<Coworker> findByEmailWithPermissions(String email);
