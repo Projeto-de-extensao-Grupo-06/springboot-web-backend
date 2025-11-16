0d88f468e8c47a9cc0ad84f4e2263b9de34bd0cc
Lara-Soares <lara.soares@sptech.school>
2025-11-16 02:54:57 -0300

    Refactor project comment and file packages
    Moved ProjectComment and ProjectFile classes to their own packages (projectComment and projectFile) and updated package declarations and imports accordingly. Also added constants and imports to ProjectService for future pagination and status filtering.

--- src/main/java/com/solarize/solarizeWebBackend/modules/project/ProjectService.java
+++ src/main/java/com/solarize/solarizeWebBackend/modules/project/ProjectService.java
@@ -1,12 +1,26 @@
 package com.solarize.solarizeWebBackend.modules.project;
 
 
+import com.solarize.solarizeWebBackend.modules.project.dto.ProjectPageDTO;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.query.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+
+import java.awt.print.Pageable;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ProjectService {
 
     private final ProjectRepository repository;
+
+    private static final int pageSize = 20;
+    private static final List<ProjectStatusEnum> HIDDEN_STATUS = Arrays.asList( ProjectStatusEnum.AWAITING_RETRY,  ProjectStatusEnum.NEGOTIATION_FAILED, ProjectStatusEnum.COMPLETED);
+
+ 
+
 }
