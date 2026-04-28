c58470710783f4f3ad5f9ed22be3492fe9d32f3f
unknown <ranier.couto@sptech.school>
2026-04-28 14:32:54 -0300

    add: Adicionando propriedade de cenário teste

--- src/main/java/com/solarize/solarizeWebBackend/shared/files/S3FileStorageStrategy.java
+++ src/main/java/com/solarize/solarizeWebBackend/shared/files/S3FileStorageStrategy.java
@@ -12,7 +12,7 @@ import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import java.io.IOException;
 
 @Component
-@Profile("dev")
+@Profile({"dev", "test"})
 public class S3FileStorageStrategy implements FileStorageStrategy {
 
     private final S3Client s3;
