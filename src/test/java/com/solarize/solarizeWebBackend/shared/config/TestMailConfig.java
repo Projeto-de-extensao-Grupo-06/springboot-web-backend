a19f33b9e9749c5bfe56b46ce0e4d4397791d0ba
unknown <ranier.couto@sptech.school>
2026-04-28 14:22:47 -0300

    add: Adicionando propriedade de cenário teste

---
+++ src/test/java/com/solarize/solarizeWebBackend/shared/config/TestMailConfig.java
@@ -0,0 +1,17 @@
+package com.solarize.solarizeWebBackend.shared.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+@Configuration
+@Profile("test")
+public class TestMailConfig {
+    
+    @Bean
+    public JavaMailSender javaMailSender() {
+        return new JavaMailSenderImpl();
+    }
+}
